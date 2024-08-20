9af295b1eed7a0a0e75724ab14faf4a6978528ae
Pramod Laha <pramodlaha.pm.08@gmail.com>
2024-08-20 21:54:20 +0545

    Updated UI

--- src/lib/Prompt.svelte
+++ src/lib/Prompt.svelte
@@ -36,10 +36,10 @@
   id="msgs-box"
 >
   <div
-    class="relative flex items-end gap-1.5 md:gap-2 w-full bg-gray-200 rounded-[26px] pt-1.5"
+    class="relative flex items-end gap-1.5 md:gap-2 w-full bg-gray-200 rounded-[26px]"
     id="prompt-box"
   >
-    <div class="flex min-w-0 flex-1 flex-col pt-4">
+    <div class="flex min-w-0 flex-1 flex-col" id="parent-div">
       <textarea
         placeholder="Enter your prompt here..."
         bind:value={prompt}
@@ -48,26 +48,28 @@
         class="bg-gray-200 m-0 resize-none border-0 bg-transparent text-token-text-primary focus:ring-0 focus-visible:ring-0 max-h-[25dvh] max-h-52 outline-none"
       ></textarea>
     </div>
-    <button
-      class="mb-2 me-1 flex h-8 w-8 items-center justify-center rounded-full bg-black text-white"
-    >
-      <svg
-        on:click={onGenerate}
-        xmlns="http://www.w3.org/2000/svg"
-        width="32"
-        height="32"
-        fill="none"
-        viewBox="0 0 32 32"
-        class="icon-2xl relative bg-gray-600 rounded-full cursor-pointer"
+    <div class="pt-1">
+      <button
+        class="mb-2 me-1 flex h-8 w-8 items-center justify-center rounded-full bg-black text-white"
       >
-        <path
-          fill="currentColor"
-          fill-rule="evenodd"
-          d="M15.192 8.906a1.143 1.143 0 0 1 1.616 0l5.143 5.143a1.143 1.143 0 0 1-1.616 1.616l-3.192-3.192v9.813a1.143 1.143 0 0 1-2.286 0v-9.813l-3.192 3.192a1.143 1.143 0 1 1-1.616-1.616z"
-          clip-rule="evenodd"
-        ></path>
-      </svg>
-    </button>
+        <svg
+          on:click={onGenerate}
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          fill="none"
+          viewBox="0 0 32 32"
+          class="icon-2xl relative bg-gray-600 rounded-full cursor-pointer"
+        >
+          <path
+            fill="currentColor"
+            fill-rule="evenodd"
+            d="M15.192 8.906a1.143 1.143 0 0 1 1.616 0l5.143 5.143a1.143 1.143 0 0 1-1.616 1.616l-3.192-3.192v9.813a1.143 1.143 0 0 1-2.286 0v-9.813l-3.192 3.192a1.143 1.143 0 1 1-1.616-1.616z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </button>
+    </div>
   </div>
 </div>
 
@@ -91,10 +93,19 @@
     background-color: transparent;
     color: #333;
     outline: none;
-    padding: 1.3rem 1.2rem;
+    padding: 1.3rem 1.5rem;
+    height: 100%;
   }
 
   textarea::-webkit-scrollbar {
     display: none;
+  }
+
+  #parent-div {
+    height: 100%;
+  }
+
+  #msgs-box{
+    padding-bottom: 1rem;
   }
 </style>
