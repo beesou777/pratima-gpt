88d0d4bb72447b90f3e23b1d5b0bad7abeca4b5e
beesou777 <sbeesou@gmail.com>
2024-08-22 07:20:40 +0545

    fix: ui issue in responsive

--- src/lib/Output.svelte
+++ src/lib/Output.svelte
@@ -20,29 +20,29 @@
   <h1 class="text-2xl font-bold py-4 px-1 text-center">Pratima AI Prompt Application</h1>
 
   {#if $isLoading || (Array.isArray($chatHistry) && $chatHistry.length >= 0)}
-    <div class="p-4 rounded max-w-[1550px] w-full" id="response">
+    <div class="md:p-4 rounded max-w-[1550px] w-full" id="response">
       <h2 class="text-lg font-semibold text-center">Write your prompt</h2>
     </div>
   {/if}
 
-  <div class="rounded max-w-[1440px] w-full {$chatHistry.length > 0 ? 'p-4' : ''}">
+  <div class="rounded w-full {$chatHistry.length > 0 ? 'md:p-4' : ''}">
     {#if Array.isArray($chatHistry) && $chatHistry.length > 0}
       {#each $chatHistry as value, index (index)}
         <div class="md:max-w-[50%] md:ml-auto max-w-full w-full">
           <span class="text-white flex justify-end font-bold">You</span>
-          <div class="user-response bg-gray-950 text-white px-10 rounded my-2">
+          <div class="user-response bg-gray-950 text-white text-end w-fit ml-auto px-3 rounded my-2">
             <p>{value.prompt}</p>
           </div>
         </div>
         <div class="md:max-w-[60%] max-w-full w-full text-balance">
           <span class="text-white font-bold">Pratima AI</span>
           {#if !value.response}
-            <div class="bg-gray-950 text-white px-10 rounded my-2">
+            <div class="bg-gray-950 text-white  px-10 rounded my-2">
               <p>Generating response ....</p>
             </div>
           {/if}
 
-          <div class="bg-gray-950 px-10 rounded {value.response ? 'py-4' : ''} relative">
+          <div class="bg-gray-950 px-3 rounded {value.response ? 'py-4' : ''} relative">
             <!-- {#if value.response.includes('<code>')}
               <button
                 class="absolute top-2 right-2 bg-blue-500 text-white px-2 py-1 rounded"
